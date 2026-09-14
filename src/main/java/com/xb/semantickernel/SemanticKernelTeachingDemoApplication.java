9019f1ad90c86d7457508ac12d6a2540dec0e924
ibqy <1274464354@qq.com>
2026-09-14 09:48:56 +0800

    chore: 添加 Maven 依赖配置和启动入口

---
+++ src/main/java/com/xb/semantickernel/SemanticKernelTeachingDemoApplication.java
@@ -0,0 +1,12 @@
+package com.xb.semantickernel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticKernelTeachingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemanticKernelTeachingDemoApplication.class, args);
+    }
+}
